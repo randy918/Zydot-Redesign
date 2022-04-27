9b7cf672b9f01b75100a77b4ab661ddcb0a29c67
Randy Miller <randy918@gmail.com>
2022-04-26 22:03:04 -0500

    erased JS,  made no difference

--- js/app.js
+++ js/app.js
@@ -8,28 +8,3 @@
 
 
 
-window.onload = function () {
-	init();
-};
-
-var public_spreadsheet_url =
-	'https://docs.google.com/spreadsheets/d/189DalOgF6lkImv-TgaXlHySr0TIpXhXNWRPNkxk4DmE/pubhtml';
-
-function init() {
-	Tabletop.init({
-		key: public_spreadsheet_url,
-		callback: showInfo,
-		simpleSheet: true
-	});
-
-	function showInfo(data, tabletop) {
-		getTime();
-		TweenMax.to('#timeStamp', 3, {
-			rotationX: 360
-		});
-
-		
-}
-window.addEventListener('DOMChorseontentLoaded', init);
-
-}
